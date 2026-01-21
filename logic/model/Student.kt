1d6730e79f67286c46a59901f5fc11496310b740
Vered-Ben-David <vered054819@gmail.com>
2026-01-21 16:11:50 +0200

    Update Student.kt
    Add initial project files

--- logic/model/Student.kt
+++ logic/model/Student.kt
@@ -1,1 +1,12 @@
+package com.example.myappstudent.model
+
+data class Student(
+    var id: String,
+    var name: String,
+    var phone: String,
+    var address: String,
+    var isChecked: Boolean = false,
+    var imageUri: String? = null,
+    val defaultImageResId: Int
+)
 
